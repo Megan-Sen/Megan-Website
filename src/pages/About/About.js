98fed26c625d58467f5795696a3b65eb6566b5e0
Megumi Yanaka <margaret_mori0102@yahoo.com>
2022-05-31 15:31:35 +0100

    working on about

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,5 +1,8 @@
 import React from "react";
 // import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaperclip } from "@fortawesome/free-solid-svg-icons";
+
 import "./About.css";
 import avatar from "./avatar.png";
 
@@ -7,9 +10,20 @@ export default function About() {
   return (
     <div className="about">
       <div className="profile">
-        <img src={avatar} alt="my-avatar" className="my-avatar"/>
+        <div className="introduce">
+          <p className="title">Profile</p>
+          <p className="title">Megan Sen</p>
+          <p>Front end developer based in the UK</p>
+          <p>Graduated a coding course Feb 2022</p>
+          <p>Internship at dijipiji in the UK since Feb 2022</p>
+          <p className="title">Skill</p>
+          <p>React</p>
+          <p className="title">Contact</p>
+          <p className="title">Detail</p>
+          CV <FontAwesomeIcon icon={faPaperclip}></FontAwesomeIcon>
+        </div>
+        <img src={avatar} alt="my-avatar" className="my-avatar" />
       </div>
-      <p></p>
     </div>
   );
 }
