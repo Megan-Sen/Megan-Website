7fd0217a923cc776f1f12179a88468516b0d77d3
Megumi Yanaka <margaret_mori0102@yahoo.com>
2022-06-12 12:03:22 +0100

    change css

--- src/pages/Works/Works.js
+++ src/pages/Works/Works.js
@@ -4,18 +4,24 @@ import "./Works.css";
 import finalproject from "./fina--project.png";
 import tvshow from "./tv-show.png";
 import cakeshop from "./cake.png";
-import dijipiji from "./dijipiji.png"
+// import dijipiji from "./dijipiji.png"
 
 export default function Works() {
   return (
     <>
       <div id="works-container">
         <div className="cyf-work">
-          <div>
-            <p className="works-title">Projects at CODE YOUR FUTURE 2021 - 2022</p>
-          </div>
+          {/* <div>
+            <p className="works-title">
+              Projects at CODE YOUR FUTURE 2021 - 2022
+            </p>
+          </div> */}
           <div className="cards-container">
-            <a href="https://sensational-life-asd.herokuapp.com/">
+            <a
+              href="https://sensational-life-asd.herokuapp.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
               <div className="work-card">
                 <p className="work-title">Final Project</p>
                 <img
@@ -25,31 +31,41 @@ export default function Works() {
                 />
               </div>
             </a>
-            <a href="https://tv-show-project-cyf.netlify.app/">
+            <a
+              href="https://tv-show-project-cyf.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
               <div className="work-card">
                 <p className="work-title">TV SHOW</p>
                 <img src={tvshow} alt="websiteimage" className="work-img" />
               </div>
             </a>
-            <div className="work-card">
-              <p className="work-title">Cake Shop Website</p>
-              <img src={cakeshop} alt="websiteimage" className="work-img" />
-            </div>
+            <a
+              href="https://cakeshop-website.netlify.app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="work-card">
+                <p className="work-title">Cake Shop Website</p>
+                <img src={cakeshop} alt="websiteimage" className="work-img" />
+              </div>
+            </a>
           </div>
         </div>
 
-        <div className="dijipiji-work">
-          <div>
+         {/* <div className="dijipiji-work"> */}
+          {/* <div>
             <p className="works-title">Internship at DIJIPIJI.LIMITED 2022</p>
-          </div>
-          <div className="cards-container">
-            <div className="work-card">
+          </div> */}
+          {/* <div className="cards-container"> */}
+            {/* <div className="work-card">
               <p className="work-title">dijipiji Website - June 2022 -</p>
               <img src={dijipiji} alt="websiteimage" className="work-img" />
-            </div>
-          </div>
-        </div>
-      </div>
+            </div> */}
+          {/* </div> */}
+        {/* </div> */}
+      </div> 
     </>
   );
 }
