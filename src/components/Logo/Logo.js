99defebd09b8a1e0925e76c3ece5947f71e7513f
Megumi Yanaka <margaret_mori0102@yahoo.com>
2022-06-15 10:17:28 +0100

    add link to logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Logo.css";
 
 function Logo() {
   return (
     <>
       <div className="logo">
-        <h1>Megan Sen</h1>
+        <Link className="link" to="/">
+          <h1>Megan Sen</h1>
+        </Link>
         {/* <h3>Front End Developer - JavaScript & React</h3> */}
       </div>
     </>
