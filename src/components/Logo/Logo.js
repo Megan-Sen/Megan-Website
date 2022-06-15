8e1cc5cd5a358f1adf1578f3a2b14b03a9c7d6fe
Megumi Yanaka <margaret_mori0102@yahoo.com>
2022-06-15 15:17:24 +0100

    make responsive

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -8,6 +8,7 @@ function Logo() {
       <div className="logo">
         <Link className="link" to="/">
           <h1>Megan Sen</h1>
+          <hr className="line-under-logo" />
         </Link>
         {/* <h3>Front End Developer - JavaScript & React</h3> */}
       </div>
